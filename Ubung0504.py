ad6dd8abd15f3d80b304741b70d2930a89e951e9
Andrey8988 <59758970+Andrey8988@users.noreply.github.com>
2020-03-10 19:33:15 +0300

    Delete Ubung0504.py

--- Ubung0504.py
+++
@@ -1,8 +0,0 @@
-diz = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
-
-with open("test04a.txt", 'a') as agrob:
-    with open("test04.txt", 'a') as grob:
-        ab = grob.read().split("\n")
-        for i in ab:
-            i = i.split("—")
-            agrob.writeline(diz[i[0]] + '—' + "\n")
